4683dc0154b25fa9fefe5b29f0a2197345acb728
luciebaker <luciebdev@gmail.com>
2019-12-24 12:02:48 -0500

    reservation section

--- src/components/home/Reservation.js
+++ src/components/home/Reservation.js
@@ -11,23 +11,23 @@ const Reservation = () => {
             
             <form id="reservation-form" method="get" action="#">
 
-            <div className="row justify-content-center">
-            <div className="col-lg-10 text-center">
-                <label for="name" className="col-md-10 unique"> Name 
+            <div className="row justify-content-center reservation">
+            <label for="name" className="col-md-6 unique"> Name 
                 <input id="name" name="name" type="text" ></input></label>
-                <label for="email" className="col-md-10 unique">Email
+                <label for="email" className="col-md-6 unique">Email
                 <input id="email" name="email" type="email" ></input></label>
-                <label for="number" className="col-md-10 unique">Contact Number
+                <label for="number" className="col-md-6 unique">Contact Number
                 <input id="number" name="number" type="text" ></input></label>
-                <label for="people" className="col-md-10 unique">How many People?
+                <label for="people" className="col-md-6 unique">How many People?
                 <input id="people" name="people" type="number" min="1" required=""></input></label>
-                <label for="date" className="col-md-10 unique">Date 
+                <label for="date" className="col-md-6 unique">Date 
                 <input id="date" name="date" type="text" data-language="en" ></input></label>
-                <label for="time" className="col-md-10 unique">Time 
+                <label for="time" className="col-md-6 unique">Time 
                 <input id="time" name="time" type="text" ></input></label>
-                <label for="request" className="col-md-10 unique">Special Requests 
-                <textarea id="request" name="request"></textarea></label>
-                <div class="col-md-10">
+                <label for="request" className="col-md-12 unique">Special Requests 
+                <textarea id="request" className="unique" name="request"></textarea></label>
+                <div className="col-lg-12 text-center">
+                <div class="col-md-12">
                 <button type="submit" className="btn-unique">Reserve my table</button>
                 </div>
             </div>
